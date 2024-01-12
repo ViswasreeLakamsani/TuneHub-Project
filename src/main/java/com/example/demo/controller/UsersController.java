4701b2e46caafb990b53e6fa00075bb323654a92
ViswasreeLakamsani <viswasreelakamsani@gmail.com>
2024-01-12 15:38:59 +0530

    First version of TuneHub

---
+++ src/main/java/com/example/demo/controller/UsersController.java
@@ -0,0 +1,66 @@
+package com.example.demo.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.example.demo.entity.Users;
+import com.example.demo.services.UsersService;
+
+import jakarta.servlet.http.HttpSession;
+
+@Controller
+public class UsersController {
+	@Autowired
+	UsersService service;
+	@PostMapping("/registration")
+	public String addUsers(@ModelAttribute Users user) {
+		//checking user exist or not through email
+		boolean  userStatus=service.emailExist(user.getEmail());
+		if(userStatus==false) {
+			service.addUser(user);
+			System.out.println("User added");
+		}
+		else {
+			System.out.println("User alraedy exists");
+		}
+		
+		
+		
+		return "home";
+	}
+	//for login
+	@PostMapping("/validate")
+	public String validate(@RequestParam("email") String email,@RequestParam("password") String password,HttpSession session) {
+		//TODO: process POST request
+		if(service.validateUser(email,password)==true) {
+			String role=service.getRole(email);
+			if(role.equals("admin")){
+					return "adminHome";
+			
+		}
+		else {
+			return "customerHome";
+		}
+		
+	}
+		else {
+			return "login";
+		}
+		}
+	
+	@GetMapping("/logout")
+	public String logout(HttpSession session) {
+		session.invalidate();
+		return "login";
+	}
+	
+	
+	
+}
+	
+
+
---
+++ src/main/java/com/example/demo/services/UsersServiceImplementation.java
@@ -0,0 +1,59 @@
+package com.example.demo.services;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.demo.entity.Users;
+import com.example.demo.repository.UsersRepository;
+@Service
+public class UsersServiceImplementation implements UsersService {
+	@Autowired
+	UsersRepository repo;
+	public String addUser(Users user) {
+		repo.save(user);
+		return "User added successfully";
+	}
+	@Override
+	public boolean emailExist(String email) {
+		if(repo.findByEmail(email)==null) {
+			return false;
+		}
+		else {
+			return true;
+		}
+		
+	}
+	@Override
+	public boolean validateUser(String email, String password) {
+		// TODO Auto-generated method stub
+		Users user=repo.findByEmail(email);
+		String db_pass=user.getPassword();
+		if(password.equals(db_pass)) {
+			return true;
+		}
+		else {
+			return false;
+		}
+		
+	}
+	@Override
+	public String getRole(String email) {
+		// TODO Auto-generated method stub
+		Users user=repo.findByEmail(email);
+		
+		return user.getRole();
+	}
+	@Override
+	public Users getUser(String email) {
+		// TODO Auto-generated method stub
+		return repo.findByEmail(email);
+	}
+	@Override
+	public void updateUser(Users user) {
+		// TODO Auto-generated method stub
+		repo.save(user);
+		
+	}
+
+}
